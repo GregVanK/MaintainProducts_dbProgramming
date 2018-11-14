fd2f931a53cd53dc216849a65eaa06fcdf12b52d
Greg V <gvankampen@upei.ca>
2018-11-14 09:59:47 -0400

    +Finalized validation

--- MaintainProducts/frmCustomerMaintenance.cs
+++ MaintainProducts/frmCustomerMaintenance.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/**
+ *File: frmCustomerMaintenance
+ *Author: Greg VanKampen
+ */
 namespace MaintainProducts
 {
     public partial class frmCustomerMaintenance : Form
     {
+        
         public frmCustomerMaintenance()
         {
             InitializeComponent();
         }
-
+        /**
+         * Saves current form to database
+         */
         private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
             this.Validate();
@@ -24,20 +31,29 @@ namespace MaintainProducts
             this.tableAdapterManager.UpdateAll(this.customerMaintenanceDataset);
 
         }
-
+        /**
+         * Loads data from dataset into form
+         */
         private void frmCustomerMaintenance_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'customerMaintenanceDataset.States' table. You can move, or remove it, as needed.
-            this.statesTableAdapter.Fill(this.customerMaintenanceDataset.States);
-            // TODO: This line of code loads data into the 'customerMaintenanceDataset.Customers' table. You can move, or remove it, as needed.
-            this.customersTableAdapter.Fill(this.customerMaintenanceDataset.Customers);
-            //Binding b =  new Binding("Value",customerMaintenanceDataset,"")
             Binding b = zipCodeTextBox.DataBindings["Text"];
             b.Format += FormatZipCode;
             b.Parse += UnFormatZipCode;
-            
-        }
+            try
+            {
+                this.statesTableAdapter.Fill(this.customerMaintenanceDataset.States);
+                this.customersTableAdapter.Fill(this.customerMaintenanceDataset.Customers);
+                
+            }
+            catch (SqlException ex)
+            {
 
+                MessageBox.Show("SQL Server Error #" + ex.Number + ";" + ex.Message);
+            }
+        }
+        /**
+         * Resets form to datasets origin values
+         */
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
             this.customersBindingSource.CancelEdit();
@@ -49,12 +65,19 @@ namespace MaintainProducts
             {
                 this.customersTableAdapter.FillByCustomerID(this.customerMaintenanceDataset.Customers, ((int)(System.Convert.ChangeType(customerIDToolStripTextBox.Text, typeof(int)))));
             }
+            catch(SqlException ex)
+            {
+                MessageBox.Show("SQL Server Error #" + ex.Number + ";" + ex.Message);
+            }
             catch (System.Exception ex)
             {
                 System.Windows.Forms.MessageBox.Show(ex.Message);
             }
 
         }
+        /**
+         * Formats Zip code with '-' after 5th number
+         */
         private void FormatZipCode(object sender, ConvertEventArgs e) {
             String modifiedZip = "";
             if (e.Value.GetType().ToString() == "System.String")
@@ -67,6 +90,9 @@ namespace MaintainProducts
                 }
             }
         }
+        /**
+         * Removes zip code formatting
+         */
         private void UnFormatZipCode(object sender, ConvertEventArgs e)
         {
             String modifiedZip = "";
--- MaintainProducts/frmProductMaintenance.cs
+++ MaintainProducts/frmProductMaintenance.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/**
+ *File: frmProductMaintenance
+ *Author: Greg VanKampen
+ */
 namespace MaintainProducts
 {
     public partial class frmProductMaintenance : Form
@@ -16,24 +20,33 @@ namespace MaintainProducts
         {
             InitializeComponent();
         }
-
+        
+        /**
+        * Saves current form to database
+        */
         private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
             this.Validate();
             this.productsBindingSource.EndEdit();
             this.tableAdapterManager.UpdateAll(this.techSupportDataSet2A);
-
         }
-
+        /**
+         * Loads data from dataset into form
+         */
         private void frmProductMaintenance_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'techSupportDataSet2A.Products' table. You can move, or remove it, as needed.
+           
             try
             {
                 this.productsTableAdapter.Fill(this.techSupportDataSet2A.Products);
-            }catch(System.Exception ex)
+            }
+            catch (SqlException ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("SQL Server Error #" + ex.Number + ";" + ex.Message);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
             }
 
         }
